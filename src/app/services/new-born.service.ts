4f408dd57d43f16aa51e4b47372e24b0f05f0ff3
abdallah <abdallah.ahmed1703091@science.menofia.edu.eg>
2025-06-24 14:04:12 +0300

    اخر تعديل

--- src/app/services/new-born.service.ts
+++ src/app/services/new-born.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
-import { NotificationService, NotificationItem } from '../services/notification.service';
+
 
 export interface Animal {
   id: number;
